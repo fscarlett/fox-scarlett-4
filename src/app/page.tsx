d67d4dffe94e18c4930974fc0a37afc524eb7597
Fox Scarlett <foxontheone@gmail.com>
2026-09-14 01:09:16 -0700

    wipe boilerplate, stub home page sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,34 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image'
+import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>
-            To get started, edit the{" "}
-            <code className={styles.code}>page.tsx</code> file.
-          </h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className={styles.container}>
+          <section>
+            <h1>Fox Scarlett</h1>
+            <h2>Web Developer</h2>
+            <p>
+              I am a web developer from California US. I&apos;ve been shipping
+              web experiences since 2015. I&apos;m committed to working with
+              design, product, and engineering team members to create high
+              quality products.
+            </p>
+          </section>
+          <section>work (dynamic cards)</section>
+          <section>skills (more interesting layout)</section>
+          <section>experience (timeline cards)</section>
+          <Image
+            className={styles.logo}
+            src='/next.svg'
+            alt='Next.js logo'
+            width={100}
+            height={20}
+            priority
+          />
         </div>
       </main>
-    </div>
-  );
+    </>
+  )
 }
