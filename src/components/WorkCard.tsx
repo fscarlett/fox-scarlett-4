ee59e5b575e2761711602428b32b35c1e4000b82
Fox Scarlett <foxontheone@gmail.com>
2026-09-19 16:48:14 -0700

    work data, work card layout

--- src/components/WorkCard.tsx
+++ src/components/WorkCard.tsx
@@ -1,10 +1,30 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 import styles from '../styles/WorkCard.module.css'
 import '../app/globals.css'
 
-function WorkCard() {
+function WorkCard({ workItem }) {
   return (
     <div className={styles['work-card']}>
-      <h3>Project Title</h3>
+      <div className={styles['content-wrapper']}>
+        <h3>{workItem.title}</h3>
+        <p>{workItem.summary}</p>
+        <p>{workItem.caseStudy}</p>
+        <Link
+          href={workItem.liveLink}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Live Link
+        </Link>
+      </div>
+      <Image
+        src={workItem.image}
+        alt={workItem.title}
+        width={600}
+        height={400}
+      />
     </div>
   )
 }
--- src/components/WorkSection.tsx
+++ src/components/WorkSection.tsx
@@ -2,18 +2,17 @@ import clsx from 'clsx'
 import WorkCard from './WorkCard'
 import styles from '../styles/WorkSection.module.css'
 import '../app/globals.css'
+import workData from '../data/work.json'
 
 function WorkSection() {
   return (
     <section className={clsx(styles['work-section'])}>
       <div className='container'>
         <h2>Work</h2>
-        <p>Dynamic cards showcasing my projects will go here.</p>
-        <WorkCard />
-        <WorkCard />
-        <WorkCard />
-        <WorkCard />
-        <WorkCard />
+        <p>These are the works .</p>
+        {workData.map((workItem, index) => (
+          <WorkCard key={index} workItem={workItem} />
+        ))}
       </div>
     </section>
   )
